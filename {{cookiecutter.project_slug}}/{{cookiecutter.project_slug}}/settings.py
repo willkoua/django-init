c3fd86d4502104fb0e37b918904266311c37491c
RignonNoel <rignon.noel@gmail.com>
2018-02-05 16:28:03 -0500

    make difference between project_name and project_slug

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = '{{cookiecutter.project_name}}.urls'
+ROOT_URLCONF = '{{cookiecutter.project_slug}}.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = '{{cookiecutter.project_name}}.wsgi.application'
+WSGI_APPLICATION = '{{cookiecutter.project_slug}}.wsgi.application'
 
 
 # Database
